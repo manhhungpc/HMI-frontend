898ab4b4766f51ce4a7c92057be0f748fe8d689c
quanghuy14219 <80802239+quanghuy14219@users.noreply.github.com>
2023-04-27 01:19:00 +0700

    Add button to bookId, update books

---
+++ src/components/BookIdButton.svelte
@@ -0,0 +1,53 @@
+<div class="list-button">
+	<div class="title">You can't see me!</div>
+	<div class="button">
+		<i class="fa-solid fa-volume-high fa-2xl" />
+	</div>
+	<div class="button">
+		<i class="fa-regular fa-bookmark fa-2xl" />
+	</div>
+	<div class="button">
+		<i class="fa-regular fa-circle-stop fa-2xl" />
+	</div>
+</div>
+
+<style lang="scss">
+	.list-button {
+		width: 120px;
+		display: flex;
+		flex-direction: column;
+		overflow-y: auto;
+		overflow-x: hidden;
+		flex-shrink: 0;
+		margin-top: 20px;
+		padding: 0 30px;
+	}
+
+	.title {
+		padding: 0 0 20px;
+		font-weight: 500;
+		font-size: 15px;
+	}
+
+	.button i {
+		display: flex;
+		align-items: center;
+		width: 45px;
+		margin-right: 16px;
+		margin-top: 30px;
+	}
+
+	.button {
+		display: flex;
+		align-items: center;
+		& + & {
+			margin-top: 20px;
+		}
+	}
+
+	@media (max-width: 1045px) {
+		.list-button {
+			display: none;
+		}
+	}
+</style>
--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import Header from './../../components/Header.svelte';
 	import { getToken } from 'src/utils/token';
+	import TitlePage from 'src/components/TitlePage.svelte';
+	import BooksReview from 'src/components/BooksReview.svelte';
 	import type { PageServerData } from './$types';
+	import Taskbar from 'src/components/Taskbar.svelte';
 
 	export let data: PageServerData;
 	const books = data;
@@ -41,5 +45,27 @@
 	};
 </script>
 
-<h1>Book</h1>
-<button type="button" on:click={onCreateBooks}>Create fake book</button>
+<!-- <h1>Book</h1>
+<button type="button" on:click={onCreateBooks}>Create fake book</button> -->
+
+<Header />
+<TitlePage />
+<div class="main-wrapper">
+	<Taskbar />
+	<BooksReview />
+</div>
+
+<style lang="scss">
+	.main-wrapper {
+		width: 100%;
+		display: flex;
+		flex-grow: 1;
+		margin-top: 100px;
+		overflow: hidden;
+	}
+	@media (max-width: 420px) {
+		.main-wrapper {
+			margin-top: 50px;
+		}
+	}
+</style>
