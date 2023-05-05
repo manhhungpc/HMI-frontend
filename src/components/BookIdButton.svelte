4ebe929f83780e4c327a153282287d58d9afa2ad
manhhungpc <phamhung2002@gmail.com>
2023-05-05 14:23:50 +0700

    reading audio function

--- src/components/BookIdButton.svelte
+++ src/components/BookIdButton.svelte
@@ -1,13 +1,49 @@
+<script lang="ts">
+	export let streamAudio = () => {};
+	export let srcAudio: string;
+
+	enum AudioState {
+		paused = 0,
+		playing = 1,
+	}
+	let audio: HTMLAudioElement,
+		state: AudioState = AudioState.playing;
+	function loadAudio(src: string) {
+		audio = new Audio(src);
+		state = AudioState.playing;
+		audio.play();
+	}
+
+	function togglePause() {
+		if (state === AudioState.paused) {
+			state = AudioState.playing;
+			audio.play();
+			return;
+		}
+		state = AudioState.paused;
+		audio.pause();
+	}
+
+	$: loadAudio(srcAudio);
+</script>
+
 <div class="list-button">
-	<div class="title">You can't see me!</div>
-	<button class="button">
+	<div class="title">Bôi đen text để nghe audio</div>
+	<button class="button" on:click={streamAudio}>
 		<i class="fa-solid fa-volume-high fa-3x" />
+	</button>
+	<!-- {#if srcAudio}
+		<audio src={srcAudio} preload="all" controls autoplay class="audio" />
+		{/if} -->
+	<button class="button" on:click={togglePause}>
+		{#if state === AudioState.playing}
+			<i class="fa-solid fa-circle-pause fa-3x" />
+		{:else}
+			<i class="fa-solid fa-forward fa-3x" />
+		{/if}
 	</button>
 	<button class="button">
 		<i class="fa-regular fa-bookmark fa-3x" />
-	</button>
-	<button class="button">
-		<i class="fa-regular fa-circle-stop fa-3x" />
 	</button>
 	<button class="button" on:click={() => (window.location.href = '/')}>
 		<i class="fa-solid fa-house fa-3x" />
--- src/routes/books/tts/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Text audio</h1>
