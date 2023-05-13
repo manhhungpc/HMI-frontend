16a19823d5136d21223fd2cb0d3bb57a905f6667
manhhungpc <phamhung2002@gmail.com>
2023-05-14 02:40:46 +0700

    matching word game for kid

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -45,7 +45,7 @@
 		<ul>
 			<li><a href="/">Home</a></li>
 			<li><a href="/books">Books</a></li>
-			<li><a href="/game">Game</a></li>
+			<li><a href="/games/words-matching">Game sắp xếp từ</a></li>
 			<li><a href="/books/64539f33f840f3e97192810f/sentences">Books sentences</a></li>
 			{#await checkToken()}
 				<p>Loading ... Plz wait sir!</p>
--- src/routes/games/words-matching/+page.svelte
+++ src/routes/games/words-matching/+page.svelte
@@ -1,213 +1,141 @@
-<div class="wrapper">
-	<h1 class="title">Shiii! Hãy lắng nghe thật kĩ nhé...<span>💬</span></h1>
-	<button class="button-start">Start Game</button>
-	<button class="button-scores">High Scores</button>
-	<div class="game-info">
-		<span />
-		<span class="current-score">Score: <span class="score" /></span>
-		<span class="time-left">Time left: <span class="timer" /></span>
-	</div>
-	<div id="container">
-		<div id="info" />
-	</div>
-	<div class="words">
-		<div class="words">
-			<span class="rural">rural</span>
-			<span class="sixth">sixth</span>
-			<span class="choir">choir</span>
-			<span class="plural">plural</span>
-			<span class="memoir">memoir</span>
-			<span class="albeit">albeit</span>
-			<span class="colonel">colonel</span>
-			<span class="penguin">penguin</span><span class="isthmus">isthmus</span>
-			<span class="anemone">anemone</span>
-			<span class="draught">draught</span><span class="brewery">brewery</span>
-			<span class="liaison">liaison</span>
-			<span class="beguile">beguile</span><span class="squirrel">squirrel</span>
+<script lang="ts">
+	import 'src/styles/gameMatching.css';
+
+	enum PlayState {
+		start = 2,
+		end = 1,
+		over = 0,
+	}
+	let token = localStorage.getItem('token') as string;
+	let questionArr: string[] = [],
+		trueAswer: string[] = [],
+		answerArr: string[] = [],
+		playState: PlayState = PlayState.end,
+		err = '',
+		score = 0;
+
+	function shuffleArray(array: string[]) {
+		let shuffled = [...array];
+		for (let i = shuffled.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			const temp = shuffled[i];
+			shuffled[i] = shuffled[j];
+			shuffled[j] = temp;
+		}
+		return shuffled;
+	}
+
+	async function getRandomQuestion() {
+		const res = await fetch(`/games/words-matching`, {
+			method: 'GET',
+			headers: {
+				'content-type': 'form-encoded',
+				Authorization: token,
+			},
+		});
+
+		const response = await res.json();
+		console.log(response);
+		if (response.status !== 200) window.location.href = '/';
+		trueAswer = response.data.tokenize;
+		questionArr = shuffleArray(response.data.tokenize);
+		return response.data;
+	}
+
+	async function startPlaying() {
+		if (playState === PlayState.over) {
+			answerArr = [];
+			trueAswer = [];
+			questionArr = [];
+			await getRandomQuestion();
+		}
+		err = '';
+		score = 0;
+		playState = PlayState.start;
+	}
+
+	function addToAnswer(index: number) {
+		answerArr = [...answerArr, questionArr[index]];
+		questionArr.splice(index, 1);
+		questionArr = questionArr;
+	}
+
+	function removeFromAnswer(index: number) {
+		questionArr = [...questionArr, answerArr[index]];
+		answerArr.splice(index, 1);
+		answerArr = answerArr;
+	}
+
+	async function sendAnswer(id: string, answer: string[]) {
+		console.log(answer, trueAswer);
+		err = '';
+		let correct =
+			answer.length === trueAswer.length && answer.every((value, i) => value === trueAswer[i]);
+		// if (answer === trueAswer) correct = true;
+		const res = await fetch(`/games/words-matching`, {
+			method: 'POST',
+			body: JSON.stringify({ sentenceId: id, correct: correct }),
+			headers: {
+				'content-type': 'form-encoded',
+				Authorization: token,
+			},
+		});
+
+		const response = await res.json();
+		if (response.status !== 200) {
+			err = 'Đáp án của bạn không đúng!';
+			return;
+		}
+		console.log(response);
+		score++;
+		answerArr = [];
+		await getRandomQuestion();
+		return;
+	}
+</script>
+
+<div class="wrapper-game">
+	{#await getRandomQuestion()}
+		<p>Đang lấy câu hỏi ...</p>
+	{:then question}
+		<h1 class="title-game">Hãy sắp xếp câu sau thành câu đúng và có nghĩa</h1>
+		<button class="button-start" on:click={startPlaying}>Bắt đầu</button>
+		<button class="button-scores" on:click={() => (playState = PlayState.over)}>Kết thúc</button>
+		<div class="game-info">
+			<span />
+			<span class="current-score">Điểm: {score}</span>
+			{#if playState === PlayState.over}
+				<span class="time-left" style="color: #fb4560;">Kết thúc!</span>
+			{/if}
 		</div>
-	</div>
-	<div id="btn-container"><button id="btn">Next</button></div>
+		{#if err}
+			<div style="color: #fb4560; font-weight: 700;">
+				{err}
+			</div>
+		{/if}
+
+		<div id="container-game">
+			<div id="box-game" />
+			<div class="wrapper-answer">
+				{#each answerArr as answer, i}
+					<button class="answer-info" on:click={() => removeFromAnswer(i)}>{answer}</button>
+				{/each}
+			</div>
+		</div>
+		{#if playState === PlayState.start}
+			<div class="words">
+				{#each questionArr as sentence, i}
+					<button on:click={() => addToAnswer(i)}>
+						{sentence}
+					</button>
+				{/each}
+			</div>
+			<button id="btn-next" on:click={() => sendAnswer(question._id, answerArr)}
+				>Câu tiếp theo</button
+			>
+		{/if}
+	{/await}
 </div>
 
 <style>
-	.wrapper {
-		/* position: relative; */
-		min-height: 100vh;
-		text-align: center;
-		margin: 0;
-		/* added to prevent x-scroll when (.words span.got) size increases from transform: scale */
-		overflow-x: hidden;
-		font-size: 1rem;
-		line-height: 1.5;
-		background-color: #ffffff;
-		background-image: url('/bg_game.jpg');
-		background-attachment: fixed;
-		background-size: cover;
-	}
-
-	/* button {
-		color: white;
-		background: var(--red);
-		padding: 1rem;
-		border: 2px solid var(--black);
-		border-radius: 5px;
-		text-decoration: none;
-		font-weight: 600;
-		font-size: clamp(1rem, 1vw + 1rem, 1.5rem);
-		transition: transform 0.2s;
-	}
-
-	button:hover {
-		transform: scale(1.1);
-	} */
-
-	:focus {
-		outline-color: var(--black);
-	}
-
-	.title {
-		color: var(--black);
-		margin-top: 0;
-		line-height: 1;
-		font-size: clamp(1.5rem, 2vw + 1.6rem, 4rem);
-		margin: 5rem 0 3rem 0;
-		position: relative;
-	}
-
-	.title span {
-		position: absolute;
-		bottom: clamp(0.75rem, 2vw + 1.3rem, 2rem);
-	}
-
-	.game-info {
-		font-size: clamp(1rem, 1vw + 1rem, 2rem);
-		font-weight: 600;
-
-		/* padding: 2rem; */
-		display: grid;
-		grid-template-columns: 15% auto auto 15%;
-	}
-
-	.time-left,
-	.current-score {
-		background: var(--grey);
-	}
-
-	.current-score {
-		border-radius: 10px 0 0 10px;
-	}
-
-	@media (max-width: 750px) {
-		.game-info {
-			grid-template-columns: 1fr 150px 150px;
-		}
-	}
-
-	.words {
-		display: flex;
-		flex-wrap: wrap;
-		margin: 5vw;
-	}
-
-	.words span {
-		color: var(--black);
-		flex: 1;
-		padding: 1rem;
-		margin: 0.25rem;
-		font-size: clamp(0.9rem, 1.1vw + 0.7rem, 3rem);
-		font-weight: 900;
-		background: var(--green);
-		border-radius: 10px;
-		box-shadow: var(--box-shadow);
-	}
-
-	.button-start,
-	.button-scores {
-		margin: 0.5rem 0.5rem;
-	}
-
-	#container {
-		display: flex;
-		flex-direction: column;
-	}
-
-	#container {
-		width: 600px;
-		margin: 15px auto;
-		text-align: center;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	#btn:hover {
-		cursor: pointer;
-	}
-
-	#info {
-		border: 1px solid black;
-		width: 100%;
-		height: 55px;
-		border-radius: 10px;
-		background-color: rgba(144, 238, 144, 0.632);
-		box-shadow: 1px 1px 5px 1px #aaa;
-		overflow: hidden;
-	}
-
-	#btn-container {
-		height: 90px;
-		bottom: 0px;
-	}
-
-	#btn {
-		width: 130px;
-		height: 70px;
-		margin: 0px auto;
-		border: none;
-		border-radius: 20%;
-		background-color: rgb(1, 162, 216);
-		z-index: 12;
-	}
-
-	#btn:focus {
-		border: none;
-		outline: none;
-	}
-
-	@media only screen and (max-width: 600px) {
-		#container {
-			margin-top: 5px;
-			width: 300px;
-		}
-
-		#info {
-			margin-top: 5px;
-			margin-bottom: 25px;
-		}
-	} /* end of media query */
-
-	.button-start,
-	.button-scores {
-		color: #ffffff;
-		background: #fb4560;
-		padding: 1rem;
-		border: 2px solid #000000;
-		border-radius: 5px;
-		text-decoration: none;
-		font-weight: 600;
-		font-size: clamp(1rem, 1vw + 1rem, 1.5rem);
-		transition: transform 0.2s;
-		font-family: inherit;
-		line-height: 1.15;
-	}
-	.words span {
-		color: #000000;
-		flex: 1;
-		padding: 1rem;
-		margin: 0.25rem;
-		font-size: clamp(0.9rem, 1.1vw + 0.7rem, 3rem);
-		font-weight: 900;
-		background: #14f0949c;
-		border-radius: 10px;
-		box-shadow: var(--box-shadow);
-	}
 </style>
